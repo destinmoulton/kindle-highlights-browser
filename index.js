2743c299315e48bec4ed04b9b00eb071557faaaa
Destin Moulton <destin.moulton@gmail.com>
2017-02-16 13:49:32 -0700

    Altered BrowserWindow title.

--- index.js
+++ index.js
@@ -15,8 +15,12 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.setTitle("Kindle Highlights Browser");
+  mainWindow = new BrowserWindow({
+    width: 800, 
+    height: 600,
+    title:"Kindle Highlights Browser"
+  });
+  
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, './www/index.html'),

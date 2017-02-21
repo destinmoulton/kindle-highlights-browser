9c34b5d65452fc0c14d27a68dc2d4a6b51280faf
Destin Moulton <destin.moulton@gmail.com>
2017-02-21 11:39:14 -0700

    Added panel note for Cmd/Ctrl.

--- react-src/components/Clips/EmptyClipList.js
+++ react-src/components/Clips/EmptyClipList.js
@@ -5,9 +5,14 @@ import { Panel } from 'react-bootstrap';
 class EmptyClipList extends Component {
     render() {
         return (
-            <Panel>
-                <h4><i className="fa fa-arrow-left"></i>&nbsp;Click on an ebook to start browsing!</h4>
-            </Panel>
+            <div>
+                <Panel>
+                    <h4><i className="fa fa-arrow-left"></i>&nbsp;Click on an ebook to start browsing!</h4>
+                </Panel>
+                <Panel>
+                    <h4><i className="fa fa-arrow-left"></i>&nbsp;Hold cmd (Mac) or Ctrl (Windows/Linux) to select multiple.</h4>
+                </Panel>
+            </div>
         );
     }
 }

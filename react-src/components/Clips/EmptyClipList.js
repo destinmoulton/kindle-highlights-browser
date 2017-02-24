c4f26239d9404bec069f20e0c88db2c1d5d02927
Destin Moulton <destin.moulton@gmail.com>
2017-02-23 17:15:22 -0700

    Added search panel to the empty clip list.

--- react-src/components/Clips/EmptyClipList.js
+++ react-src/components/Clips/EmptyClipList.js
@@ -7,10 +7,13 @@ class EmptyClipList extends Component {
         return (
             <div>
                 <Panel>
-                    <h4><i className="fa fa-arrow-left"></i>&nbsp;Click on an ebook to start browsing!</h4>
+                    <h4><i className="fa fa-search"></i>&nbsp;Use the search box to find specific authors or titles.</h4>
                 </Panel>
                 <Panel>
-                    <h4><i className="fa fa-arrow-left"></i>&nbsp;Hold cmd (Mac) or Ctrl (Windows/Linux) to select multiple.</h4>
+                    <h4><i className="fa fa-arrow-left"></i>&nbsp;Click on an Author or Title to start browsing.</h4>
+                </Panel>
+                <Panel>
+                    <h4><i className="fa fa-plus-square"></i>&nbsp;Hold cmd (Mac) or Ctrl (Windows/Linux) to select multiple authors or titles.</h4>
                 </Panel>
             </div>
         );

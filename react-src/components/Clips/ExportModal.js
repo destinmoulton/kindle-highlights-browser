b3bde107644f539cfd6b06dc6e9ec966f86c29da
Destin Moulton <destin.moulton@gmail.com>
2017-05-02 15:02:34 -0600

    Added radios to the save and copy functions.

--- react-src/components/Clips/ExportModal.js
+++ react-src/components/Clips/ExportModal.js
@@ -101,7 +101,7 @@ class ClipsExportModal extends Component {
         const possibleTitle = FILE_PREFIX+FILE_DATE_SUFFIX;
         remote.dialog.showSaveDialog({defaultPath:possibleTitle+FILE_EXT}, (filename)=>{
             if(typeof filename === "string"){
-                const strToWrite = GenerateClipsString(this.props.clips, this.state.checkboxes, this.state.separators, EOL);
+                const strToWrite = GenerateClipsString(this.props.clips, this.state.checkboxes, this.state.radios, this.state.separators, EOL);
                 fs.writeFile(filename, strToWrite, function(err){
                     if(err){
                         return ErrorDialog("There was a problem saving the file.\n"+err);
@@ -115,7 +115,7 @@ class ClipsExportModal extends Component {
      * Copy generated clip string to the clipboard.
      */
     handleCopyClips(){
-        const strToCopy = GenerateClipsString(this.props.clips, this.state.checkboxes, this.state.separators, EOL);
+        const strToCopy = GenerateClipsString(this.props.clips, this.state.checkboxes, this.state.radios, this.state.separators, EOL);
 
         clipboard.writeText(strToCopy);
     }

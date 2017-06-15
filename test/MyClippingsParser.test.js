14f3fe65aa3460eb8b706f8e5d96d5b9c2c5a6fa
Destin Moulton <destin.moulton@gmail.com>
2017-06-15 14:59:23 -0600

    Changed str to the more specific locationAndDate

--- test/MyClippingsParser.test.js
+++ test/MyClippingsParser.test.js
@@ -1,7 +1,10 @@
 import path from 'path';
-import MyClippingsParser from '../react-src/lib/MyClippingsParser';
 
+import _ from 'lodash';
+
+import MyClippingsParser from '../react-src/lib/MyClippingsParser';
 import EXPECTED_CLIPPINGS from './data/MyClippings.expected';
+
 
 const TEST_FILE = path.resolve(__dirname + '/data/', 'MyClippings.test.txt');
 
@@ -11,4 +14,34 @@ test("parses a test clips file", ()=>{
     const clippings = myClippingsParser.parseFile(TEST_FILE);
     const clippingsJSON = JSON.stringify(clippings);
     expect(clippingsJSON).toBe(EXPECTED_CLIPPINGS);
+});
+
+describe("determineAuthorName parses the author's name", ()=>{
+    const EXPECTED_ONEPART_NAME = "Prince";
+    const EXPECTED_TWOPART_NAME = "Jason Bourne";
+    const EXPECTED_UNDEFINED_NAME = "";
+    let myClippingsParser = {};
+    beforeEach(()=>{
+        myClippingsParser = new MyClippingsParser();
+    });
+    
+    test('last, first', ()=>{
+        const testName = "Bourne, Jason";
+        expect(myClippingsParser.determineAuthorName(testName)).toBe(EXPECTED_TWOPART_NAME);
+    });
+
+    test('first last', ()=>{
+        const testName = "Jason Bourne";
+        expect(myClippingsParser.determineAuthorName(testName)).toBe(EXPECTED_TWOPART_NAME);
+    });
+
+    test('one-name', ()=>{
+        const testName = "Prince";
+        expect(myClippingsParser.determineAuthorName(testName)).toBe(EXPECTED_ONEPART_NAME);
+    });
+
+    test('no-name', ()=>{
+        const testName = "";
+        expect(myClippingsParser.determineAuthorName(testName)).toBe(EXPECTED_UNDEFINED_NAME);
+    })
 });

cfed6ed7bc0ef5d1092792a1f4ae17699a2bc96e
Destin Moulton <destin.moulton@gmail.com>
2017-06-19 11:25:41 -0600

    Fixed the sortClips ascending test.

---
+++ test/ClipSorter.test.js
@@ -0,0 +1,64 @@
+import ClipSorter from '../react-src/lib/ClipSorter';
+
+describe("ClipSorter", ()=>{
+    const CLIPS_ORIGINAL = [
+        {
+            location_start: 10
+        },
+        {
+            location_start: 5
+        },
+        {
+            location_start: 3
+        },
+        {
+            location_start: 15
+        }
+    ]; 
+
+    test("sortClips ascending", ()=>{
+        const FILTER = "location_start|asc";
+        const EXPECTED_ASC = [
+            {
+                location_start: 3
+            },
+            {
+                location_start: 5
+            },
+            {
+                location_start: 10
+            },
+            {
+                location_start: 15
+            }
+        ];
+
+        let clipsToSort = [...CLIPS_ORIGINAL];
+        const clipSorter = new ClipSorter();
+        clipSorter.sortClips(clipsToSort, FILTER);
+        expect(clipsToSort).toEqual(EXPECTED_ASC);
+    });
+
+    test("sortClips descending", ()=>{
+        const FILTER = "location_start|desc";
+        const EXPECTED_DESC = {
+            "15": {
+                location_start: 15
+            },
+            "10": {
+                location_start: 10
+            },
+            "5": {
+                location_start: 5
+            },
+            "3": {
+                location_start: 3
+            }
+        };
+
+        let clipsToSort = Object.create(CLIPS_ORIGINAL);
+        const clipSorter = new ClipSorter();
+        clipSorter.sortClips(clipsToSort, FILTER);
+        expect(clipsToSort).toEqual(EXPECTED_DESC);
+    });
+});

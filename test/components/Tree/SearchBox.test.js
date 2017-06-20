e6434f23b9750f0d36814649ee63b31afb7ccc66
Destin Moulton <destin.moulton@gmail.com>
2017-06-20 09:49:07 -0600

    Added test for icon setup when search string is set.

--- test/components/Tree/SearchBox.test.js
+++ test/components/Tree/SearchBox.test.js
@@ -1,22 +1,33 @@
+import React from 'react';
 
 import { mount } from 'enzyme';
 
 import SearchBox from '../../../react-src/components/Tree/SearchBox.js';
 
 describe('Tree/SearchBox Component', ()=>{
+    const SEARCH_STRING = "Mobius Strip";
     let props = {};
     let enzymeWrapper = {};
     beforeEach(()=>{
         props = {
             handleSearchChange: jest.fn(),
-            originalSearchString: "Mobius",
+            originalSearchString: SEARCH_STRING,
             handleClearSearch: jest.fn()
         };
 
         enzymeWrapper = mount(<SearchBox {...props}/>)
     });
 
-    test("should render the search box and clear x", ()=>{
-       expect(enzymeWrapper.find('FormControl').exists()).toBe(true);
+    test("should render the search text input box with search string", ()=>{
+        const formControl = enzymeWrapper.find('input.form-control');
+        expect(formControl.exists()).toBe(true);
+        expect(formControl.prop('value')).toBe(SEARCH_STRING);
     });
+
+    test("icon/button is X and set to clear function when search text is set", ()=>{
+        const iconWrapper = enzymeWrapper.find('span.input-group-addon');
+        expect(iconWrapper.exists()).toBe(true);
+        expect(iconWrapper.hasClass("khb-treelist-search-close-icon")).toBe(true);
+
+    })
 });

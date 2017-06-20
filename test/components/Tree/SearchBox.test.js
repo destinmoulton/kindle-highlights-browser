a69dba8b11467b2b976f65885d4fb18be932b2d5
Destin Moulton <destin.moulton@gmail.com>
2017-06-20 10:04:45 -0600

    Re-organized the search wrappers into a new describe section.

--- test/components/Tree/SearchBox.test.js
+++ test/components/Tree/SearchBox.test.js
@@ -5,41 +5,52 @@ import { mount } from 'enzyme';
 import SearchBox from '../../../react-src/components/Tree/SearchBox.js';
 
 describe('Tree/SearchBox Component', ()=>{
-    const SEARCH_STRING = "Mobius Strip";
-    let props = {};
-    let enzymeWrapper = {};
-    beforeEach(()=>{
-        props = {
-            handleSearchChange: jest.fn(),
-            originalSearchString: SEARCH_STRING,
-            handleClearSearch: jest.fn()
-        };
 
-        enzymeWrapper = mount(<SearchBox {...props}/>)
-    });
+    describe("search string is set", ()=>{
+        const SEARCH_STRING = "Mobius Strip";
+        let props = {};
+        let enzymeWrapper = {};
+        beforeEach(()=>{
+            props = {
+                handleSearchChange: jest.fn(),
+                originalSearchString: SEARCH_STRING,
+                handleClearSearch: jest.fn()
+            };
 
-    test("should render the search text input box with search string", ()=>{
-        const formControl = enzymeWrapper.find('input.form-control');
-        expect(formControl.exists()).toBe(true);
-        expect(formControl.prop('value')).toBe(SEARCH_STRING);
-    });
-
-    describe("clear/X icon", ()=>{
-        test("icon/button is X and set to clear function when search text is set", ()=>{
-            const iconWrapper = enzymeWrapper.find('span.input-group-addon');
-            expect(iconWrapper.exists()).toBe(true);
-            expect(iconWrapper.hasClass("khb-treelist-search-close-icon")).toBe(true);
-
-            const icon = iconWrapper.find('i');
-            expect(icon.exists()).toBe(true);
-            expect(icon.hasClass("fa-times")).toBe(true);
+            enzymeWrapper = mount(<SearchBox {...props}/>)
         });
-        
-        test("fires handleClearSearch fn when clicked", ()=>{
-            expect(props.handleClearSearch.mock.calls.length).toBe(0);
-            icon.props().onClick();
-            expect(props.handleClearSearch.mock.calls.length).toBe(1);
 
-        })
+        test("should render the search text input box with search string", ()=>{
+            const formControl = enzymeWrapper.find('input.form-control');
+            expect(formControl.exists()).toBe(true);
+            expect(formControl.prop('value')).toBe(SEARCH_STRING);
+        });
+
+        describe("clear/X icon", ()=>{
+            let iconWrapper = {};
+            let icon = {};
+
+            beforeEach(()=>{
+                iconWrapper = enzymeWrapper.find('span.input-group-addon');
+                icon = iconWrapper.find('i');
+            });
+
+            test("wrapper exists and class is set", ()=>{
+                expect(iconWrapper.exists()).toBe(true);
+                expect(iconWrapper.hasClass("khb-treelist-search-close-icon")).toBe(true);
+            });
+            
+            test("exists as i and X icon is set", ()=>{
+                expect(icon.exists()).toBe(true);
+                expect(icon.hasClass("fa-times")).toBe(true);
+            });
+
+            test("fires handleClearSearch fn when clicked", ()=>{
+                expect(props.handleClearSearch.mock.calls.length).toBe(0);
+                icon.props().onClick();
+                expect(props.handleClearSearch.mock.calls.length).toBe(1);
+
+            })
+        });
     });
 });

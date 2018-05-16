d79141e58561dabf2d82eba7094f9b5692331761
Destin Moulton <destin.moulton@gmail.com>
2018-05-16 15:20:26 -0600

    Renamed app.js to App.js.

---
+++ react-src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+    render
+} from "react-dom";
+import {
+    Route
+} from "react-router";
+import {
+    BrowserRouter
+} from "react-router-dom";
+
+import Layout from "./components/Layout.js";
+
+import "./electron_menus/rightclick";
+
+class Main {
+    run() {
+        const container = document.getElementById("react-app");
+
+        const routing = ( <
+            BrowserRouter >
+
+            <
+            Route path = "/"
+            component = {
+                Layout
+            }
+            /> <
+            /BrowserRouter>
+        );
+
+        render(routing, container);
+    }
+}
+
+var main = new Main();
+main.run();

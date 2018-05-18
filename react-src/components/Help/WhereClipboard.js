175c9b2495bfaf3f0a3a0ddf120597120edf0552
Destin Moulton <destin.moulton@gmail.com>
2018-05-18 11:18:42 -0600

    Added more info for locating My Clippings.

--- react-src/components/Help/WhereClipboard.js
+++ react-src/components/Help/WhereClipboard.js
@@ -4,14 +4,19 @@ import { Panel } from "react-bootstrap";
 class WhereClipboard extends Component {
     render() {
         return (
-            <Panel>
-                <h4>Where is "My Clippings.txt"?</h4>
-                <p>"My Clippings.txt" is a file on your Kindle.</p>
+            <Panel className="khb-home-panel">
+                <h4>What is "My Clippings.txt"?</h4>
+                <p>
+                    "My Clippings.txt" is a file on your Kindle that contains
+                    your highlights and notes.
+                </p>
                 <h4>How do I find it?</h4>
                 <ol>
                     <li>Plug your Kindle into your computer.</li>
                     <li>Click the Open button above.</li>
                     <li>Navigate to your Kindle.</li>
+                    <li>Click the file named "My Clippings.txt".</li>
+                    <li>Click Open.</li>
                 </ol>
             </Panel>
         );

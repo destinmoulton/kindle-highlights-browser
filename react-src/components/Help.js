55ebed15851baca323e616ad13a5ad2566cd3d7a
Destin Moulton <destin.moulton@gmail.com>
2018-05-23 13:40:20 -0600

    Refactored Help into function component.

--- react-src/components/Help.js
+++ react-src/components/Help.js
@@ -2,14 +2,12 @@ import React from "react";
 
 import WhereAreMyClippings from "./Help/WhereAreMyClippings";
 
-export default class Help extends React.Component {
-    render() {
-        let helpContents = [];
-
-        return (
-            <div>
-                <WhereAreMyClippings />
-            </div>
-        );
-    }
+function Help() {
+    return (
+        <div>
+            <WhereAreMyClippings />
+        </div>
+    );
 }
+
+export default Help;

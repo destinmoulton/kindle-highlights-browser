a24bc6838143bea3fc5216c5437f37c66185b010
Destin Moulton <destin.moulton@gmail.com>
2018-05-23 13:38:39 -0600

    Refactored into function component.

--- react-src/components/Tree/SearchBox.js
+++ react-src/components/Tree/SearchBox.js
@@ -3,49 +3,44 @@ import PropTypes from "prop-types";
 
 import { FormControl, FormGroup, InputGroup } from "react-bootstrap";
 
-class SearchBox extends Component {
-    static propTypes = {
-        handleSearchChange: PropTypes.func,
-        originalSearchString: PropTypes.string,
-        handleClearSearch: PropTypes.func
-    };
+function SearchBox(props) {
+    const {
+        handleSearchChange,
+        originalSearchString,
+        handleClearSearch
+    } = props;
 
-    render() {
-        const {
-            handleSearchChange,
-            originalSearchString,
-            handleClearSearch
-        } = this.props;
-
-        let iconClassName = "fa fa-search";
-        let iconOnChange = handleSearchChange;
-        let addonClassName = "";
-        if (originalSearchString != "") {
-            addonClassName = "khb-treelist-search-close-icon";
-            iconClassName = "fa fa-times";
-            iconOnChange = handleClearSearch;
-        }
-        return (
-            <div id="khb-treelist-search-container">
-                <FormGroup>
-                    <InputGroup>
-                        <FormControl
-                            type="text"
-                            placeholder="Search Authors/Titles..."
-                            value={originalSearchString}
-                            onChange={handleSearchChange}
-                        />
-                        <InputGroup.Addon className={addonClassName}>
-                            <i
-                                className={iconClassName}
-                                onClick={iconOnChange}
-                            />
-                        </InputGroup.Addon>
-                    </InputGroup>
-                </FormGroup>
-            </div>
-        );
+    let iconClassName = "fa fa-search";
+    let iconOnChange = handleSearchChange;
+    let addonClassName = "";
+    if (originalSearchString != "") {
+        addonClassName = "khb-treelist-search-close-icon";
+        iconClassName = "fa fa-times";
+        iconOnChange = handleClearSearch;
     }
+    return (
+        <div id="khb-treelist-search-container">
+            <FormGroup>
+                <InputGroup>
+                    <FormControl
+                        type="text"
+                        placeholder="Search Authors/Titles..."
+                        value={originalSearchString}
+                        onChange={handleSearchChange}
+                    />
+                    <InputGroup.Addon className={addonClassName}>
+                        <i className={iconClassName} onClick={iconOnChange} />
+                    </InputGroup.Addon>
+                </InputGroup>
+            </FormGroup>
+        </div>
+    );
 }
+
+SearchBox.propTypes = {
+    handleSearchChange: PropTypes.func,
+    originalSearchString: PropTypes.string,
+    handleClearSearch: PropTypes.func
+};
 
 export default SearchBox;

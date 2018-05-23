79a1ee3bd2e491e0efce15992b65f29f4c37ff9d
Destin Moulton <destin.moulton@gmail.com>
2018-05-23 13:37:22 -0600

    Refactored into function component.

--- react-src/components/Tree/Node.js
+++ react-src/components/Tree/Node.js
@@ -1,39 +1,37 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-class Node extends Component {
-    static propTypes = {
-        itemName: PropTypes.string,
-        handleChangeSelectedFilter: PropTypes.func,
-        filterFieldName: PropTypes.string,
-        filters: PropTypes.object
-    };
+function Node(props) {
+    const {
+        itemName,
+        handleChangeSelectedFilter,
+        filterFieldName,
+        filters
+    } = props;
 
-    render() {
-        const {
-            itemName,
-            handleChangeSelectedFilter,
-            filterFieldName,
-            filters
-        } = this.props;
-
-        let className = "khb-treelist-node";
-        if (filters.has(filterFieldName, itemName)) {
-            className += " khb-treelist-activenode";
-        }
-        return (
-            <li key={itemName}>
-                <div
-                    data-filter-field={filterFieldName}
-                    data-filter-content={itemName}
-                    className={className}
-                    onClick={handleChangeSelectedFilter}
-                >
-                    {itemName}
-                </div>
-            </li>
-        );
+    let className = "khb-treelist-node";
+    if (filters.has(filterFieldName, itemName)) {
+        className += " khb-treelist-activenode";
     }
+    return (
+        <li key={itemName}>
+            <div
+                data-filter-field={filterFieldName}
+                data-filter-content={itemName}
+                className={className}
+                onClick={handleChangeSelectedFilter}
+            >
+                {itemName}
+            </div>
+        </li>
+    );
 }
+
+Node.propTypes = {
+    itemName: PropTypes.string,
+    handleChangeSelectedFilter: PropTypes.func,
+    filterFieldName: PropTypes.string,
+    filters: PropTypes.object
+};
 
 export default Node;

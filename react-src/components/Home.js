b68a4f01dcdcf33e014a50c829773dc89d2979c3
Destin Moulton <destin.moulton@gmail.com>
2019-02-14 09:57:27 -0700

    Migrated Panel to Card.

--- react-src/components/Home.js
+++ react-src/components/Home.js
@@ -1,17 +1,20 @@
 import React from "react";
 
-import { Col, Button, Panel } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
 
 import Help from "./Help";
 
 function Home(props) {
     return (
         <Col lg={12} md={12} sm={12}>
-            <Panel className="khb-home-panel text-center">
-                <Button onClick={props.openClippingsDialogHandler}>
-                    Open "My Clippings.txt"...
-                </Button>
-            </Panel>
+            <Card className="khb-home-panel text-center">
+                <Card.Body>
+                    <Button onClick={props.openClippingsDialogHandler}>
+                        Open "My Clippings.txt"...
+                    </Button>
+                </Card.Body>
+            </Card>
             <Help />
         </Col>
     );

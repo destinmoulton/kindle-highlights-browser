d2fff458801473ea035b8c6d1b5fb3d409532d9b
Destin Moulton <destin.moulton@gmail.com>
2019-02-14 09:54:28 -0700

    Migrated Panel to Card

--- react-src/components/Help/WhereAreMyClippings.js
+++ react-src/components/Help/WhereAreMyClippings.js
@@ -1,24 +1,26 @@
 import React, { Component } from "react";
-import { Panel } from "react-bootstrap";
 
+import Card from "react-bootstrap/Card";
 class WhereAreMyClippings extends Component {
     render() {
         return (
-            <Panel className="khb-home-panel">
-                <h4>What is "My Clippings.txt"?</h4>
-                <p>
-                    "My Clippings.txt" is a file on your Kindle that contains
-                    your highlights and notes.
-                </p>
-                <h4>How do I find it?</h4>
-                <ol>
-                    <li>Plug your Kindle into your computer.</li>
-                    <li>Click the Open button above.</li>
-                    <li>Navigate to your Kindle.</li>
-                    <li>Click the file named "My Clippings.txt".</li>
-                    <li>Click Open.</li>
-                </ol>
-            </Panel>
+            <Card className="khb-home-panel">
+                <Card.Body>
+                    <h4>What is "My Clippings.txt"?</h4>
+                    <p>
+                        "My Clippings.txt" is a file on your Kindle that
+                        contains your highlights and notes.
+                    </p>
+                    <h4>How do I find it?</h4>
+                    <ol>
+                        <li>Plug your Kindle into your computer.</li>
+                        <li>Click the Open button above.</li>
+                        <li>Navigate to your Kindle.</li>
+                        <li>Click the file named "My Clippings.txt".</li>
+                        <li>Click Open.</li>
+                    </ol>
+                </Card.Body>
+            </Card>
         );
     }
 }

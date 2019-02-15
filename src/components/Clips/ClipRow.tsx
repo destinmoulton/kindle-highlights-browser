7555b2accea331d15bf582ab824daddde06fbdfd
Destin Moulton <destin.moulton@gmail.com>
2019-02-15 16:54:12 -0700

    Converted to react function; added prop types.

--- src/components/Clips/ClipRow.tsx
+++ src/components/Clips/ClipRow.tsx
@@ -1,56 +1,50 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import * as React from "react";
+import * as Types from "../../types";
+interface Props {
+    clip: Types.Clip;
+}
+export default (props: Props) => {
+    const { clip } = this.props;
+    let loc = "";
+    let icon = null;
+    let quote = "";
 
-class ClipRow extends Component {
-    render() {
-        const { clip } = this.props;
-        let loc = "";
-        let icon = "";
-        let quote = "";
-
-        if (clip.location.type === "highlight") {
-            loc = "Highlight at ";
-            icon = (
-                <i
-                    className="fa fa-quote-left khb-clipslist-icon"
-                    aria-hidden="true"
-                />
-            );
-            quote = `"`;
-        } else if (clip.location.type === "note") {
-            loc = "Note at ";
-            icon = (
-                <i
-                    className="fa fa-sticky-note-o khb-clipslist-icon"
-                    aria-hidden="true"
-                />
-            );
-        }
-
-        loc += `Location: ${clip.location.value}`;
-
-        return (
-            <div>
-                <div>
-                    {icon}&nbsp;
-                    <span className="khb-clipslist-cliploc">{loc}</span> --{" "}
-                    <span className="khb-clipslist-clipdate">
-                        {clip.date.format("MMMM DD, YYYY h:mm:ss a")}
-                    </span>
-                </div>
-                <div className="khb-clipslist-cliptext">
-                    {quote}
-                    {clip.text}
-                    {quote}
-                </div>
-                <hr />
-            </div>
+    if (clip.location.type === "highlight") {
+        loc = "Highlight at ";
+        icon = (
+            <i
+                className="fa fa-quote-left khb-clipslist-icon"
+                aria-hidden="true"
+            />
+        );
+        quote = `"`;
+    } else if (clip.location.type === "note") {
+        loc = "Note at ";
+        icon = (
+            <i
+                className="fa fa-sticky-note-o khb-clipslist-icon"
+                aria-hidden="true"
+            />
         );
     }
-}
 
-ClipRow.propTypes = {
-    clip: PropTypes.object
+    loc += `Location: ${clip.location.value}`;
+
+    return (
+        <div>
+            <div>
+                {icon}&nbsp;
+                <span className="khb-clipslist-cliploc">{loc}</span> --{" "}
+                <span className="khb-clipslist-clipdate">
+                    {clip.date.format("MMMM DD, YYYY h:mm:ss a")}
+                </span>
+            </div>
+            <div className="khb-clipslist-cliptext">
+                {quote}
+                {clip.text}
+                {quote}
+            </div>
+            <hr />
+        </div>
+    );
 };
-
-export default ClipRow;

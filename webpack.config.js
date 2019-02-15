ff0c0457107c765249ffa0bff8a76b81ce8db886
Destin Moulton <destin.moulton@gmail.com>
2019-02-15 16:12:45 -0700

    Modified source to point to tsx app.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require("path");
 
 module.exports = {
-    entry: "./src/App.jsx",
+    entry: "./src/App.tsx",
     devtool: "source-map",
     output: {
         path: path.join(__dirname, "www/js"),

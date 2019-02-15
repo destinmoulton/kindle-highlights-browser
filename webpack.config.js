23530031dfadda1e678f7a4244794607aa42d9a3
Destin Moulton <destin.moulton@gmail.com>
2019-02-15 13:59:16 -0700

    Removed babel and added typescript packages and config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,35 @@
 var path = require("path");
 
 module.exports = {
-    entry: "./react-src/App.js",
+    entry: "./react-src/App.jsx",
     devtool: "source-map",
     output: {
         path: path.join(__dirname, "www/js"),
         filename: "khb-app.js"
     },
 
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
     module: {
         rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            }
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
     },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        react: "React",
+        "react-dom": "ReactDOM"
     },
-    plugins: [],
     target: "electron-main"
 };

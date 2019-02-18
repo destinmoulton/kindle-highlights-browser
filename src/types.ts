1ee33a822c77a6d94c493093380397a4b49fb1af
Destin Moulton <destin.moulton@gmail.com>
2019-02-18 09:48:32 -0700

    Added filtered clips type and export types.

--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,7 @@ import * as moment from "moment";
 export type ClipKey = number;
 
 export interface Clip {
+    [key: string]: string | Location | number | moment.Moment;
     title: string;
     authorFullName: string;
     location: Location;
@@ -12,9 +13,48 @@ export interface Clip {
     text: string;
 }
 
-export type Clippings = Map<ClipKey, Clip>;
+export type ClippingsMap = Map<ClipKey, Clip>;
+
+export interface FilteredClips {
+    [key: string]: Clip[];
+}
 
 export interface Location {
     type: string;
     value: string;
+}
+
+export type Authors = string[];
+export type Titles = string[];
+
+export interface Filter {
+    fieldName: string;
+    cleanContent: string;
+    dirtyContent: string;
+}
+
+export type Filters = Filter[];
+
+export interface ExportCheckboxes {
+    [key: string]: boolean;
+    location: boolean;
+    date: boolean;
+    quote: boolean;
+}
+
+export interface ExportSeparators {
+    [key: string]: string;
+    title: string;
+    clip: string;
+}
+
+export interface ExportRadios {
+    [key: string]: string;
+    clip_separator: string;
+}
+
+export interface ExportOptions {
+    checkboxes: ExportCheckboxes;
+    separators: ExportSeparators;
+    radios: ExportRadios;
 }

38db27d37288e95a51590c843e8031b04e672113
Destin Moulton <destin.moulton@gmail.com>
2019-02-21 14:54:28 -0700

    Created the basic prefix/suffix component.

---
+++ src/components/Modals/ExportModal/PrefixSuffixTab.tsx
@@ -0,0 +1,74 @@
+import * as React from "react";
+
+import {
+    Button,
+    ButtonGroup,
+    Col,
+    Form,
+    InputGroup,
+    Row
+} from "react-bootstrap";
+
+import * as Types from "../../../types";
+
+interface Props {
+    inputValues: Types.ExportPrefixSuffixElements;
+    onChangeValue: (id: string, position: string, value: string) => void;
+}
+
+class PrefixSuffixTab extends React.Component<Props> {
+    _prePostInputs = [
+        { id: "location", name: "Location/Date" },
+        { id: "highlight", name: "Highlight" },
+        { id: "note", name: "Note" }
+    ];
+
+    constructor(props: Props) {
+        super(props);
+    }
+
+    _handleOnChange(id: string, position: string, evt: any) {
+        this.props.onChangeValue(id, position, evt.target.value);
+    }
+
+    _renderInputGroups() {
+        const { inputValues } = this.props;
+
+        return this._prePostInputs.map(input => {
+            return (
+                <InputGroup size="sm">
+                    <InputGroup.Prepend>
+                        <InputGroup.Text>{input.name}</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                        type="text"
+                        placeholder="Prefix"
+                        onChange={this._handleOnChange.bind(
+                            this,
+                            input.id,
+                            "prefix"
+                        )}
+                        value={inputValues[input.id]["prefixValue"]}
+                    />
+                    <Form.Control
+                        type="text"
+                        placeholder="Suffix"
+                        onChange={this._handleOnChange.bind(
+                            this,
+                            input.id,
+                            "suffix"
+                        )}
+                        value={inputValues[input.id]["suffixValue"]}
+                    />
+                </InputGroup>
+            );
+        });
+    }
+
+    render() {
+        const inputGroups = this._renderInputGroups();
+        return <div>{inputGroups}</div>;
+    }
+}
+
+export default PrefixSuffixTab;

3b18a41b467a8dd2b614016ac35989f811bc43b8
Destin Moulton <destin.moulton@gmail.com>
2019-02-27 17:09:59 -0700

    Added EOL Select form.

--- src/components/Modals/ExportModal/PrefixSuffixTab.tsx
+++ src/components/Modals/ExportModal/PrefixSuffixTab.tsx
@@ -1,13 +1,6 @@
 import * as React from "react";
 
-import {
-    Button,
-    ButtonGroup,
-    Col,
-    Form,
-    InputGroup,
-    Row
-} from "react-bootstrap";
+import { Button, ButtonGroup, Col, Form, Row } from "react-bootstrap";
 
 import * as Types from "../../../types";
 
@@ -36,38 +29,84 @@ class PrefixSuffixTab extends React.Component<Props> {
 
         return this._prePostInputs.map(input => {
             return (
-                <InputGroup key={input.id} size="sm">
-                    <InputGroup.Prepend>
-                        <InputGroup.Text>{input.name}</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <Form.Control
-                        type="text"
-                        placeholder="Prefix"
-                        onChange={this._handleOnChange.bind(
-                            this,
-                            input.id,
-                            "prefixValue"
-                        )}
-                        value={inputValues[input.id]["prefixValue"]}
-                    />
-                    <Form.Control
-                        type="text"
-                        placeholder="Suffix"
-                        onChange={this._handleOnChange.bind(
-                            this,
-                            input.id,
-                            "suffixValue"
-                        )}
-                        value={inputValues[input.id]["suffixValue"]}
-                    />
-                </InputGroup>
+                <Form.Group as={Row} key={input.id} size="sm">
+                    <Col xs="6">
+                        <Form.Label column>{input.name}</Form.Label>
+                    </Col>
+                    <Col xs="6">
+                        <Form.Control
+                            type="text"
+                            size="sm"
+                            placeholder="Prefix"
+                            onChange={this._handleOnChange.bind(
+                                this,
+                                input.id,
+                                "prefixValue"
+                            )}
+                            value={inputValues[input.id]["prefixValue"]}
+                        />
+                        <Form.Control
+                            type="text"
+                            size="sm"
+                            placeholder="Suffix"
+                            onChange={this._handleOnChange.bind(
+                                this,
+                                input.id,
+                                "suffixValue"
+                            )}
+                            value={inputValues[input.id]["suffixValue"]}
+                        />
+                    </Col>
+                </Form.Group>
+            );
+        });
+    }
+
+    _renderEOLSelects() {
+        const { inputValues } = this.props;
+
+        let options: React.ReactElement[] = [];
+        for (let i = 0; i <= 10; i++) {
+            options.push(<option key={i}>{i}</option>);
+        }
+
+        return this._prePostInputs.map(input => {
+            return (
+                <Form.Group as={Row} key={input.id} size="sm">
+                    <Col xs="6">
+                        <Form.Label column>{input.name}</Form.Label>
+                    </Col>
+                    <Col sm="6">
+                        <Form.Control
+                            as="select"
+                            onChange={this._handleOnChange.bind(
+                                this,
+                                input.id,
+                                "suffixEOL"
+                            )}
+                            defaultValue={inputValues[input.id]["suffixEOL"]}
+                        >
+                            {options}
+                        </Form.Control>
+                    </Col>
+                </Form.Group>
             );
         });
     }
 
     render() {
         const inputGroups = this._renderInputGroups();
-        return <div>{inputGroups}</div>;
+        const suffixEOLs = this._renderEOLSelects();
+        return (
+            <div>
+                <br />
+                <h5>Prefix/Suffix Text</h5>
+                {inputGroups}
+                <br />
+                <h5>Extra Lines</h5>
+                {suffixEOLs}
+            </div>
+        );
     }
 }
 

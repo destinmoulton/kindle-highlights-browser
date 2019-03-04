e27230bca55b8a8d9b3e99731023654f173491e7
Destin Moulton <destin.moulton@gmail.com>
2019-03-04 13:38:48 -0700

    Created option form generator.

---
+++ src/components/Modals/ExportModal/Tabs/OptionFormGenerator.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import { Col, Form, Row } from "react-bootstrap";
+import * as Types from "../../../../types";
+interface Props {
+    group_id: string;
+    group_name: string;
+    elements: Types.ExportOptionFormElements;
+    changeElement: (evt: any) => void;
+}
+class FormGenerator extends React.Component<Props> {
+    constructor(props: Props) {
+        super(props);
+    }
+
+    _renderCheckbox(element: Types.ExportOptionFormElement) {
+        const isChecked = element.value === "true" || element.value === true;
+        return (
+            <Form.Check
+                key={element.id}
+                inline
+                checked={isChecked}
+                onChange={this.props.changeElement}
+                value={element.id}
+                type="checkbox"
+                label="Include Location"
+            />
+        );
+    }
+
+    _renderText(element: Types.ExportOptionFormElement) {
+        const placeholder = element.placeholder ? element.placeholder : "";
+        return (
+            <Form.Group as={Row} key={element.id} size="sm">
+                <Col xs="6">
+                    <Form.Label column>{element.name}</Form.Label>
+                </Col>
+                <Col xs="6">
+                    <Form.Control
+                        type="text"
+                        size="sm"
+                        placeholder={placeholder}
+                        onChange={this.props.changeElement}
+                        value={element.value.toString()}
+                    />
+                </Col>
+            </Form.Group>
+        );
+    }
+
+    render() {
+        const { elements } = this.props;
+
+        return Object.values(elements).map(element => {
+            switch (element.type) {
+                case "checkbox":
+                    return this._renderCheckbox(element);
+                case "text":
+                    return this._renderText(element);
+            }
+        });
+    }
+}
+
+export default FormGenerator;

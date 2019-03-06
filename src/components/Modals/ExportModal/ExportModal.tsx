1f39fda6a30fb446384fffba81dcedc028cf69ab
Destin Moulton <destin.moulton@gmail.com>
2019-03-06 10:44:10 -0700

    Migrated the buttons to the preview tab.

--- src/components/Modals/ExportModal/ExportModal.tsx
+++ src/components/Modals/ExportModal/ExportModal.tsx
@@ -1,33 +1,13 @@
-import * as fs from "fs";
-import { EOL } from "os";
-
 import * as React from "react";
 
-import {
-    Button,
-    ButtonGroup,
-    Col,
-    Modal,
-    Row,
-    Tabs,
-    Tab
-} from "react-bootstrap";
-
-import { clipboard, remote } from "electron";
-import * as moment from "moment";
+import { Button, Col, Modal, Row, Tabs, Tab } from "react-bootstrap";
 
 import OptionsTab from "./Tabs/OptionsTab";
 import PreviewTab from "./Tabs/PreviewTab";
 
-import PreviewGenerator from "../../../lib/PreviewGenerator";
-import { errorDialog } from "../../../lib/errorDialog";
 import * as Types from "../../../types";
 
 import EXPORT_OPTIONS from "../../../data/exportOptions";
-
-const FILE_PREFIX = "kindle_highlights_";
-const FILE_DATE_SUFFIX = moment().format("MM_DD_YYYY_HH_mm_ss");
-const FILE_EXT = ".txt";
 
 interface Props {
     modalIsActive: boolean;
@@ -65,58 +45,6 @@ class ExportModal extends React.Component<Props, State> {
         });
     };
 
-    /**
-     * Show the file save dialog box and save the clip
-     * string to the selected file.
-     */
-    handleSaveClipsToFile() {
-        const possibleTitle = FILE_PREFIX + FILE_DATE_SUFFIX;
-        remote.dialog.showSaveDialog(
-            { defaultPath: possibleTitle + FILE_EXT },
-            filename => {
-                if (typeof filename === "string") {
-                    const previewGenerator = new PreviewGenerator(
-                        this.state.exportOptions,
-                        EOL
-                    );
-                    const strToWrite = previewGenerator.generate(
-                        this.props.filteredClips
-                    );
-                    fs.writeFile(filename, strToWrite, function(err) {
-                        if (err) {
-                            return errorDialog(
-                                "There was a problem saving the file.\n" + err
-                            );
-                        }
-                    });
-                }
-            }
-        );
-    }
-
-    /**
-     * Copy generated clip string to the clipboard.
-     */
-    handleCopyClips() {
-        const previewGenerator = new PreviewGenerator(
-            this.state.exportOptions,
-            EOL
-        );
-        const strToCopy = previewGenerator.generate(this.props.filteredClips);
-
-        clipboard.writeText(strToCopy);
-    }
-
-    /**
-     * Select all the clips in the textarea.
-     */
-    handleSelectAll() {
-        const el: HTMLInputElement = document.getElementById(
-            "khb-exportpreview-textarea"
-        ) as HTMLInputElement;
-        el.select();
-    }
-
     render() {
         const { modalIsActive, closeModalHandler, filteredClips } = this.props;
         const { exportOptions } = this.state;
@@ -151,19 +79,6 @@ class ExportModal extends React.Component<Props, State> {
                         </Col>
                     </Row>
                     <Row>
-                        <ButtonGroup>
-                            <Button
-                                onClick={this.handleSaveClipsToFile.bind(this)}
-                            >
-                                <i className="fa fa-save" /> Save All to File
-                            </Button>
-                            <Button onClick={this.handleCopyClips.bind(this)}>
-                                <i className="fa fa-clipboard" /> Copy All
-                            </Button>
-                            <Button onClick={this.handleSelectAll.bind(this)}>
-                                <i className="fa fa-hand-rock" /> Select All
-                            </Button>
-                        </ButtonGroup>
                         <Button
                             onClick={closeModalHandler}
                             className="pull-right"
--- src/components/Modals/ExportModal/Tabs/PreviewTab.tsx
+++ src/components/Modals/ExportModal/Tabs/PreviewTab.tsx
@@ -1,29 +1,110 @@
+import * as fs from "fs";
+import { EOL } from "os";
 import * as React from "react";
+import { Button, ButtonGroup, Col, Form, Row } from "react-bootstrap";
+import { clipboard, remote } from "electron";
+import * as moment from "moment";
 
-import { Form } from "react-bootstrap";
+import { errorDialog } from "../../../../lib/errorDialog";
+import PreviewGenerator from "../../../../lib/PreviewGenerator";
 import * as Types from "../../../../types";
 
-import PreviewGenerator from "../../../../lib/PreviewGenerator";
 interface Props {
     exportOptions: Types.ExportOptions;
     filteredClips: Types.FilteredClips;
 }
 const TEXTAREA_EOL = "\n";
+const FILE_PREFIX = "kindle_highlights_";
+const FILE_DATE_SUFFIX = moment().format("MM_DD_YYYY_HH_mm_ss");
+const FILE_EXT = ".txt";
 
-function PreviewTab(props: Props) {
-    const { filteredClips, exportOptions } = props;
+class PreviewTab extends React.Component<Props> {
+    /**
+     * Show the file save dialog box and save the clip
+     * string to the selected file.
+     */
+    handleSaveClipsToFile() {
+        const possibleTitle = FILE_PREFIX + FILE_DATE_SUFFIX;
+        remote.dialog.showSaveDialog(
+            { defaultPath: possibleTitle + FILE_EXT },
+            filename => {
+                if (typeof filename === "string") {
+                    const previewGenerator = new PreviewGenerator(
+                        this.props.exportOptions,
+                        EOL
+                    );
+                    const strToWrite = previewGenerator.generate(
+                        this.props.filteredClips
+                    );
+                    fs.writeFile(filename, strToWrite, function(err) {
+                        if (err) {
+                            return errorDialog(
+                                "There was a problem saving the file.\n" + err
+                            );
+                        }
+                    });
+                }
+            }
+        );
+    }
 
-    const previewGenerator = new PreviewGenerator(exportOptions, TEXTAREA_EOL);
+    /**
+     * Copy generated clip string to the clipboard.
+     */
+    handleCopyClips() {
+        const previewGenerator = new PreviewGenerator(
+            this.props.exportOptions,
+            EOL
+        );
+        const strToCopy = previewGenerator.generate(this.props.filteredClips);
 
-    const clipsPreview = previewGenerator.generate(filteredClips);
+        clipboard.writeText(strToCopy);
+    }
 
-    return (
-        <Form>
-            <Form.Group controlId="khb-exportmodal-preview-textarea">
-                <Form.Control as="textarea" value={clipsPreview} readOnly />
-            </Form.Group>
-        </Form>
-    );
+    /**
+     * Select all the clips in the textarea.
+     */
+    handleSelectAll() {
+        const el: HTMLInputElement = document.getElementById(
+            "khb-exportmodal-preview-textarea"
+        ) as HTMLInputElement;
+        el.select();
+    }
+    render() {
+        const { filteredClips, exportOptions } = this.props;
+
+        const previewGenerator = new PreviewGenerator(
+            exportOptions,
+            TEXTAREA_EOL
+        );
+
+        const clipsPreview = previewGenerator.generate(filteredClips);
+
+        return (
+            <Form>
+                <Form.Group controlId="khb-exportmodal-preview-textarea">
+                    <Form.Control as="textarea" value={clipsPreview} readOnly />
+                </Form.Group>
+                <Form.Group as={Row}>
+                    <Col sm={{ span: 10, offset: 2 }}>
+                        <ButtonGroup>
+                            <Button
+                                onClick={this.handleSaveClipsToFile.bind(this)}
+                            >
+                                <i className="fa fa-save" /> Save All to File
+                            </Button>
+                            <Button onClick={this.handleCopyClips.bind(this)}>
+                                <i className="fa fa-clipboard" /> Copy All
+                            </Button>
+                            <Button onClick={this.handleSelectAll.bind(this)}>
+                                <i className="fa fa-hand-rock" /> Select All
+                            </Button>
+                        </ButtonGroup>
+                    </Col>
+                </Form.Group>
+            </Form>
+        );
+    }
 }
 
 export default PreviewTab;
